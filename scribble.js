fe499e4b6cda18d222df569045ef0d093a377279
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-06-13 10:06:37 -0500

    sampling on number of points

--- scribble.js
+++ scribble.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var INTERVAL = 500;
+  var SKIPS = 2;
 
   var myLatlng = new google.maps.LatLng(40, -88);
   var myOptions = {
@@ -10,7 +10,6 @@ $(document).ready(function() {
 
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
   
-  var drawing = false;
   scribbles = [];
   var currentListener;
   var currentTimeOut;
@@ -23,8 +22,8 @@ $(document).ready(function() {
   google.maps.event.addListener(map, 'click', function(e) {
     
       map.setOptions({draggable: false});
-
-      drawing = false;  
+      
+      var count = 0;  
       var p = new google.maps.Polyline({
         map: map,
         path: [e.latLng]
@@ -33,25 +32,18 @@ $(document).ready(function() {
       scribbles.push(p);
       
       currentListener = google.maps.event.addListener(map, 'mousemove', function(e) {
-        if (drawing) {
+        if ((count % SKIPS) == 0) {
           var path = p.getPath();
           path.push(e.latLng); // pushing to path automatically updates the line
           drawing = false;
         }
+        count++;
       });
-
-      var loop = function() {
-        drawing = true;
-        currentTimeOut = setTimeout(loop, INTERVAL);
-      }
-
-      currentTimeOut = setTimeout(loop, INTERVAL); 
 
       google.maps.event.addListener(p, 'click', function(e) {
         drawing = false;
         google.maps.event.removeListener(currentListener);       
         map.setOptions({draggable: true });
-        clearTimeout(currentTimeOut);
       });
   });
     
