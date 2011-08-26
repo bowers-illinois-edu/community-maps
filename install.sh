e6518e4fb049c0066f0772579f50b75251c44995
Ubuntu <ubuntu@ip-10-39-97-241.ec2.internal>
2011-08-26 19:03:00 +0000

    installing postgre/postgis

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+################################################################################
+# Installing Software for Amazon EC2 Instance
+################################################################################
+
+apt-get -y install postgresql-8.4-postgis
+apt-get -y install postgis
+
+
+
