9294286d7d0e5d5d22e57e8720543ce2a8008ffc
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-09-19 14:25:51 -0500

    two page survey with no questions

--- src/community_maps/core.clj
+++ src/community_maps/core.clj
@@ -1,1 +1,34 @@
-(ns community-maps.core)
+(ns community-maps.core
+  (:use [shanks core appengine-magic]
+        hiccup.page-helpers
+        [burp.ring :only [wrap-burp]]
+        [burp.jquery :only [jquery-link jquery-ui-link]])
+  (:require [appengine-magic.core :as ae]))
+
+(defn randomizer []
+  (randomize-subject {:condition [true false]}))
+
+(defn createwithid []
+  (let [key (dbsave (randomizer))]
+    (dbload (.getId key))))
+
+(defn layout [body]
+  (xhtml
+   [:head
+    [:title "Taking a survey"]
+    (jquery-link) (jquery-ui-link)
+    (include-js "burp.jquery.ui.support.js")]
+   [:body body]))
+
+(defscreen thank-you [_] "Thank you for taking this survey.")
+
+(defscreen draw-on-map [subject] [:div "this is a test"])
+
+(def survey-app
+  (wrap-burp (survey createwithid dbsave dbload layout thank-you [draw-on-map])))
+
+(ae/def-appengine-app survey-app-ae #'survey-app)
+
+
+
+
