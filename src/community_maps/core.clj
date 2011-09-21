a1b4ee00fb641992106f59865e79b4ec80f8d8c9
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-09-21 16:56:52 -0500

    additional styles for rank questions

--- src/community_maps/core.clj
+++ src/community_maps/core.clj
@@ -63,7 +63,12 @@
 (defscreen draw-on-map
   [subject]
   (question "Please draw on the map"
-   (scribble-map :test-scribble 40.1105556 -88.2072222)))
+            (scribble-map :test-scribble 40.1105556 -88.2072222))
+  (rank-options :icecream "Rank the following ice cream flavors:"
+                {:choc "Chocolate"
+                 :van "Vanilla"
+                 :straw "Strawberry"
+                 :rock "Rocky Road"}))
 
 (def survey-app
   (-> (survey createwithid dbsave dbload layout thank-you [draw-on-map])
