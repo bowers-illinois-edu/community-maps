92c0c42860c40d104e0ccb2716dfd6e9ea9dcf21
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-09-28 18:26:58 -0500

    more questions

--- src/community_maps/screens/everything.clj
+++ src/community_maps/screens/everything.clj
@@ -159,42 +159,54 @@ areas you highlighted")
     {:local-community "People in your local community"
      :census-community "People in this area"}))
 
+  (directions "Now let’s look at the map of your local community again. [REFERRING TO MAP THEY DREW ON]")
+
 ;;;Q22.	Question:
-;;;Now let’s look at the map of your local community again. [REFERRING TO MAP THEY DREW ON]
-;;; 
-;;;Thinking about your local community: is it mostly white, mostly ethnic minorities, about half and half, or some other mixture of people? 
-;;; 
-;;;[IF R RESPONDS A MIXTURE: Can you explain what you mean by “some other mixture”?]
-;;; 
+  (question 
+   "Thinking about your local community: is it mostly white, mostly ethnic minorities, about half and half, or some other mixture of people?"
+   (bf/radio-group :community-ethnic-makeup
+                   {:white "Mostly white"
+                    :ethnic "Mostly ethnic minorities"
+                    :half "About half and half"
+                    :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
-;;; 
-;;; 
 ;;; 
 ;;;Q23.	Question: 
-;;;[REFERRING TO THE MAP THEY DREW ON]
-;;;Thinking about your local community: is it mostly conservatives, mostly liberals, mostly NDP, or some other mixture? [if Quebec, add "mostly Bloc Quebecois"]
-;;; 
-;;;[IF R RESPONDS A MIXTURE: Can you explain what you mean by “some other mixture”?]
-;;; 
-;;; 
-;;; 
-;;; 
-;;;
+  (single-choice
+   :community-political-makeup
+   "Thinking about your local community, is it mostly:"
+   {:conservatives "Conservatives"
+    :liberals "Liberals"
+    :ndp "NDP"
+    :quebec "Bloc Quebecois"
+    :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]})
+  
 ;;;Q24.	Question:
-;;;Are any of your friends of a different race or ethnic background than you?
+  (yes-no :ethnic-friends "Are any of your friends of a different race or ethnic background than you?")
 ;;; 
 ;;;If yes, then Q24a
-;;; 
 ;;;Q24a. Question:
-;;;Are your friends mostly white, mostly ethnic minorities, about half and half, or some other mixture of people?
-;;; 
-;;;[IF R RESPONDS A MIXTURE: Can you explain what you mean by “some other mixture”?]
-;;; 
+  (question 
+   "Are your friends mostly white, mostly ethnic minorities, about half and half, or some other mixture of people?"
+   (bf/radio-group
+    :ethnic-friends-composition
+    {:white "Mostly white"
+     :ethnic "Mostly ethnic minorities"
+     :half "About half and half"
+     :other [:span "Some other mixture. Please explain:" (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
-;;; 
-;;; 
+
 ;;;Q25.	Question:
-;;;Are your friends mostly conservatives, mostly liberals, mostly NDP, or some other mixture? [if Quebec, add "mostly Bloc Quebecois"]
+  (question 
+   "Are your friends mostly conservatives, mostly liberals, mostly NDP, or some other mixture? [if Quebec, add \"mostly Bloc Quebecois\"]"
+   (bf/radio-group
+    :political-friends-composition
+    {:conservative "Mostly conservatives"
+     :liberal "Mostly liberal"
+     :ndp "Mostly NDP"
+     :quebec "Mostly Bloc Quebecois"
+     :other [:span "Some other mixture. Please explain:" (f/text-field :other-description)]}))
+
 ;;; 
 ;;;[IF R RESPONDS A MIXTURE: Can you explain what you mean by “some other mixture”?]
 ;;; 
