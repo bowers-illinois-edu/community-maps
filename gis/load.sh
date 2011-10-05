7ed398beaff3c8869d8c3c80bfe0ad770f11974d
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-10-05 11:10:32 -0500

    starting to turn GIS data into KML files

---
+++ gis/load.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Preconditions:
+# - a Postgres/Postgis database called `canada`
+# - all the varioius district files in a subdirectory called `shapefile`:
+#   gccs000a06a_e.shp
+#   gcd_000a06a_e.shp
+#   gcma000a06a_e.shp
+#   gcsd000a06a_e.shp
+#   gct_000a06a_e.shp
+#   gdpl000a06a_e.shp
+#   gfed000a06a_e.shp
+#   gfsa000a06a_e.shp
+#   gpr_000a06a_e.shp
+#   gua_000a06a_e.shp
+# 
