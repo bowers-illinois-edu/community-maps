8e09b7d04f58f9b8cf7f6f386d19613a154e8e61
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-10-05 13:27:40 -0500

    load files into db, create basic .kml files for each row in each table

---
+++ gis/create-kml.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby -w
+
+require 'rubygems'
+require 'postgres'
+
+# example query
+# select asKML(ST_simplifypreservetopology(the_geom,0.001)) from gccs000a06a_e limit 1; 
+#
+# Tables:
+# ccs 
+# cd 
+# cma 
+# csd 
+# ct
+# dpl 
+# fed 
+# fsa 
+# pr
+# ua
+
+# Some constants for use in the data -> .kml process
+SIMPLIFY = 0.001 # how much should the geomtries be simplified before ouput
+TABLES = ['ccs', 'cd', 'cma', 'csd', 'ct', 'dpl', 'fed', 'fsa', 'pr', 'ua']
+# NOTE: the tables all have an id called TABLENAMEuid, with the exception of fsa and ua
+# There the ids are census_fsa and uapid
+
+def select(table)
+  id = table + "uid"
+  if (table == 'fsa')
+    id = 'census_fsa'
+  elsif (table == 'ua')
+    id = 'uapuid'
+  end
+
+  "SELECT #{id}, asKML(ST_SimplifyPreserveTopology(ST_transform(the_geom, 3857), #{SIMPLIFY})) FROM #{table} LIMIT 2"
+end
+
+# DB connection
+USER = 'mark'
+DB = 'canada'
+db = PGconn.connect(:dbname => DB, :user => USER)
+
+if (!FileTest::directory?("kml"))
+  Dir::mkdir("kml")
+end
+
+TABLES.map {|t|
+  if (!FileTest::directory?("kml/" + t))
+    Dir::mkdir("kml/" + t)
+  end
+
+ db.query(select(t)).map {|q|
+  id = q[0]
+  kml = q[1]
+  File.open("kml/#{t}/#{id}.kml", 'w') {|f|
+    tmp = <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+<Document>
+<Placemark>
+#{kml}
+</Placemark>
+</Document>
+</kml>
+EOF
+    f.write(tmp)
+  }
+ }
+  
+}
+
--- gis/load.sh
+++ gis/load.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Preconditions:
-# - a Postgres/Postgis database called `canada`
-# - all the varioius district files in a subdirectory called `shapefile`:
+# - a Postgres/Postgis database called `canada`, LATIN1 encoding is necessary
+# (e.g. `$ createdb -T postgis-template canada`)
+# - all the varioius district files in a subdirectory called `files`:
 #   gccs000a06a_e.shp
 #   gcd_000a06a_e.shp
 #   gcma000a06a_e.shp
@@ -13,4 +14,26 @@
 #   gfsa000a06a_e.shp
 #   gpr_000a06a_e.shp
 #   gua_000a06a_e.shp
-# 
+# The .gitignore file will ignore `gis/files` so that you can load stuff there without committing it to the repository (for size concerns).
+
+### Projection Info
+# All the Canda files use:
+# GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]
+# This appears to be EPSG: 4269
+#
+# Google uses EPSG:3857
+
+cd files
+
+shp2pgsql -s 4269 -I -W LATIN1 gccs000a06a_e.shp ccs | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gcd_000a06a_e.shp cd | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gcma000a06a_e.shp cma | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gcsd000a06a_e.shp csd | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gct_000a06a_e.shp ct | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gdpl000a06a_e.shp dpl | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gfed000a06a_e.shp fed | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gfsa000a06a_e.shp fsa | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gpr_000a06a_e.shp pr | psql canada
+shp2pgsql -s 4269 -I -W LATIN1 gua_000a06a_e.shp ua | psql canada
+
+  
