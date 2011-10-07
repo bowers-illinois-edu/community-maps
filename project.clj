ffaba11a7d0a951e8a9e07bc093d8102c63ff52e
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-10-07 16:22:20 -0500

    latest version of burp

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Community maps survey experiment"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [shanks "0.1.2"]
-                 [burp "0.1.1"]
+                 [burp "0.1.2"]
                  [hiccup "0.3.6"]]
   :dev-dependencies [[appengine-magic "0.4.4"]])
 
