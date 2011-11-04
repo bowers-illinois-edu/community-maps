1ce0b734de8a1ba9ecbfd81e0e38ce8b086aa975
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-11-04 11:01:16 -0500

    fetching GIS files

--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@
 apt-get -y install postgresql-8.4-postgis
 apt-get -y install postgis
 
+mkdir -p gis/files
+rsync -e sssh -Avz drubenson@direct.jakebowers.org:"~/CanadaContext/boundary\ files" gis/files
+
+
 
 
