ceedb1f67ab54aaab8b8f3f1ef1969705951ebf1
markmfredrickson <mark.m.fredrickson@gmail.com>
2011-11-14 15:59:19 -0600

    creating the postgis db

--- install.sh
+++ install.sh
@@ -21,6 +21,13 @@ unzip "gis/files/boundary files/gfsa000a06a_e.zip" -d gis/files
 unzip "gis/files/boundary files/gpr_000a06a_e.zip" -d gis/files
 unzip "gis/files/boundary files/gua_000a06a_e.zip" -d gis/files
 
+### Creating the Postgresql user and database
+sudo -u postgres createdb postgis-template
+sudo -u postgres createlang plpgsql postgis-template
 
+POSTGIS_SQL_PATH=/usr/share/postgresql/9.1/contrib/postgis-1.5/
+sudo -u postgres psql -d postgis-template -f $POSTGIS_SQL_PATH/postgis.sql
+sudo -u postgres psql -d postgis-template -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
 
+sudo -u postgres createdb -T postgis-template canada
 
