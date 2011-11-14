20efbe6872bec289dfebb5e26051bffbd609b5b9
markmfredrickson <mark.m.fredrickson@gmail.com>
2011-11-14 14:35:47 -0600

    updated version of postgis, fetch and unzip gis files

--- install.sh
+++ install.sh
@@ -4,11 +4,22 @@
 # Installing Software for Amazon EC2 Instance
 ################################################################################
 
-apt-get -y install postgresql-8.4-postgis
-apt-get -y install postgis
+sudo apt-get -y install postgresql-9.1-postgis
+sudo apt-get -y install postgis
 
 mkdir -p gis/files
-rsync -e sssh -Avz drubenson@direct.jakebowers.org:"~/CanadaContext/boundary\ files" gis/files
+rsync -e ssh -r -Avz drubenson@direct.jakebowers.org:"~/CanadaContext/boundary\ files" gis/files
+
+unzip "gis/files/boundary files/gccs000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gcd_000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gcma000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gcsd000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gct_000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gdpl000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gfed000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gfsa000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gpr_000a06a_e.zip" -d gis/files
+unzip "gis/files/boundary files/gua_000a06a_e.zip" -d gis/files
 
 
 
