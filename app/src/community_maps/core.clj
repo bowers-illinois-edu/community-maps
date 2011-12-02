ed5cc6eb240d2db85e82d5f889cba854b9fb6138
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-12-02 11:29:11 -0600

    updated to latest burp and shanks, updated layout accordingly
    Conflicts:
    
    	app/src/community_maps/core.clj

--- app/src/community_maps/core.clj
+++ app/src/community_maps/core.clj
@@ -27,7 +27,21 @@
   (let [key (dbsave (randomizer))]
     (dbload (.getId key))))
 
-(defn layout [body]
+(defmulti layout (fn [subject screen] screen))
+
+(def css (list
+          (include-css "http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css")
+          (include-css "http://yui.yahooapis.com/2.8.2r1/build/base/base-min.css")
+          (include-css "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/themes/ui-lightness/jquery-ui.css")
+          (include-css "local.css")))
+
+(defn body [title body]
+  [:body {:id "doc" :class "yui-t7"}
+    [:div#hd [:h1 title]]
+    [:div#bd
+     [:div.yui-g body]]])
+  
+(defmethod layout :default [subject screen]
   (xhtml
    [:head
     [:title "Taking a survey"]
@@ -39,20 +53,33 @@
     (include-js "utilities.js")
     (include-js "questions.js")
     (include-js "http://maps.google.com/maps/api/js?v=3.4&sensor=false")
-    (include-css "http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css")
-    (include-css "http://yui.yahooapis.com/2.8.2r1/build/base/base-min.css")
-    (include-css "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/themes/ui-lightness/jquery-ui.css")
-    (include-css "local.css")]
-   [:body {:id "doc" :class "yui-t7"}
-    [:div#hd [:h1 "Hello!"]]
-    [:div#bd
-     [:div.yui-g body]]]))
+    css]
+   (body (str "Survey Step " (:step subject 0)) (screen-form screen subject))))
 
-; screens defined in screens.* namespaces
+;; other screens defined in screens.* namespaces
+;; the thank you screen is special.
 (defscreen thank-you [_] "Thank you for taking this survey.")
+(defmethod layout thank-you [subject screen]
+  (xhtml
+   [:head
+    [:title "Thank You"]
+    css]
+   (body "Thank You" (screen subject))))
+
+;; Consent also gets a special layout function
+(defmethod layout consent [subject screen]
+  (xhtml
+   [:head
+    [:title "Welcome"]
+    (jquery-link)
+    (jquery-ui-link)
+    (include-js "burp.jquery.ui.support.js")
+    (include-js "questions.js")
+    css]
+   (body "Welcome" (screen-form screen subject))))
 
 (def survey-app
-  (-> (survey createwithid dbsave dbload layout thank-you
+  (-> (survey createwithid dbsave dbload layout 
               [consent
                address
                draw
@@ -61,7 +88,8 @@
                canada-population
                racial-ethnic
                racial-conflict
-               minorities-community])
+               minorities-community
+               thank-you])
       wrap-burp))
 
 (ae/def-appengine-app community-maps-app #'survey-app)
