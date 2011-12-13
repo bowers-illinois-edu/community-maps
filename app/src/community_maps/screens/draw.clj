96516bcc4dc8e54f5b8cf4bc0be36c2038e2526b
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-12-13 13:58:38 -0600

    bullet points, closes #82

--- app/src/community_maps/screens/draw.clj
+++ app/src/community_maps/screens/draw.clj
@@ -11,5 +11,11 @@
     (question
      (list
       [:span.required "Please Draw Your Local Community on the Map. "]
-      "On the map, please draw the boundaries of your local community using your mouse. Click Start Drawing to begin your drawing. Hold down the mouse button to make a line. Open shapes will be enclosed if you lift your mouse for a few seconds. You may make as many shapes as you like. Even though we have centered the map on your address, you should also feel free to zoom in or out and to move the map if it does not contain the areas that you consider your local community. If you left click on a completed shape you will have the chance to delete that community. You may Reset the Map to delete all of your shapes.")
+      [:ul (map #(vector :li %)
+                ["Click \"Start Drawing\" to begin drawing your community."
+                 "Hold down the mouse button to make a line. Open shapes will be enclosed if you lift your mouse for a few 		seconds."
+                 "You may make as many shapes as you like."
+                 "Even though we have centered the map on your postal code, you should feel free to zoom in or out and to move 		the map if it does not contain the areas that you consider your local community."
+                 "If you left click on a completed shape you will have the chance to delete that community."
+                 "You may Erase the Map to delete all of your shapes."])])
               (scribble-map :community lat lng))))
