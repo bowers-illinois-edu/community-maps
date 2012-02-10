e821072f98c2829650defbbd8fc10f01cebbd95a
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-02-10 15:56:48 -0600

    adding 'mostly' qualifer to housing, comma. fixes #136

--- app/src/community_maps/screens/everything.clj
+++ app/src/community_maps/screens/everything.clj
@@ -225,7 +225,7 @@
                     :not-at-all-worried "Not at all worried"}))
 
   (question
-   "If you could find housing that you liked, would you rather live with neighbors who mostly share your political beliefs and values or who hold a wide range of political beliefs and values, or is it not important to you?"
+   "If you could find housing that you liked, would you rather live with neighbors who mostly share your political beliefs and values, or who hold a wide range of political beliefs and values, or is it not important to you?"
    (bf/radio-group
     :housing-political
     {:share "Mostly share my political beliefs and values"
@@ -233,12 +233,11 @@
      :not-important "This is not important to me"}))
   
   (question
-   
-   "What about when it comes to the race and ethnicity of your neighbors? If you could find housing that you liked, would you prefer neighbors who share your racial and ethnic background or who represent a mix of racial and ethnic backgrounds, or is it not important to you?"
+   "What about when it comes to the race and ethnicity of your neighbors? If you could find housing that you liked, would you prefer neighbors who share your racial and ethnic background, or who represent a mix of racial and ethnic backgrounds, or is it not important to you?"
    (bf/radio-group
     :housing-ethnic
-    {:same "Share my racial and ethnic background"
-     :diversity "A mix of racial and ethnic backgrounds"
+    {:same "Mostly share my racial and ethnic background"
+     :diversity "Mostly a mix of racial and ethnic backgrounds"
      :not-important "This is not important to me"}))
 
   (directions
