4ff3c06dcd4258c9c5e1660ed5d67de4adb4f09a
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-02-10 11:30:58 -0600

    other pol groups last, closes #140

--- app/src/community_maps/screens/minorities_community.clj
+++ app/src/community_maps/screens/minorities_community.clj
@@ -6,51 +6,54 @@
 
 (defscreen minorities-community
   [subject]
-  (static-map-communities subject)
+  (let [polgroups (conj (shuffle (vec (political-groups subject)))
+          [ :other
+           [:span "Some other mixture. Please explain: " (f/text-field :other-description)]])]
+    (list (static-map-communities subject)
 
-  (directions "Now let’s look at the map of your local community again.")
+          (directions "Now let’s look at the map of your local community again.")
 
-  
+          
 ;;;Q22.	Question:
-  (question 
-   "Thinking about your local community again: is it mostly white, mostly racial and ethnic minorities, about half and half, or some other mixture of people?"
-   (bf/radio-group :community-ethnic-makeup
-                   {:white "Mostly white"
-                    :ethnic "Mostly racial and ethnic minorities"
-                    :half "About half and half"
-                    :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]}))
+          (question 
+           "Thinking about your local community again: is it mostly white, mostly racial and ethnic minorities, about half and half, or some other mixture of people?"
+           (bf/radio-group :community-ethnic-makeup
+                           {:white "Mostly white"
+                            :ethnic "Mostly racial and ethnic minorities"
+                            :half "About half and half"
+                            :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
 ;;; 
 ;;;Q23.	Question: 
-  (single-choice
-   :community-political-makeup
-   "Thinking about your local community, is it mostly:"
-   (assoc (political-groups subject)
-     :other
-     [:span "Some other mixture. Please explain: " (f/text-field :other-description)]))
-  
+          (question
+           "Thinking about your local community, is it mostly:"
+           (bf/radio-group
+            :community-political-makeup
+            polgroups))
+          
 ;;;Q24.	Question:
-  (yes-no :ethnic-friends "Are any of your friends of a different race or ethnic background than you?")
+          (yes-no :ethnic-friends "Are any of your friends of a different race or ethnic background than you?")
 ;;; 
 ;;;If yes, then Q24a
 ;;;Q24a. Question:
-  (question 
-   "Are your friends mostly white, mostly racial and ethnic minorities, about half and half, or some other mixture of people?"
-   (bf/radio-group
-    :ethnic-friends-composition
-    {:white "Mostly white"
-     :ethnic "Mostly racial and ethnic minorities"
-     :half "About half and half"
-     :other [:span "Some other mixture. Please explain:" (f/text-field :other-description)]}))
+          (question 
+           "Are your friends mostly white, mostly racial and ethnic minorities, about half and half, or some other mixture of people?"
+           (bf/radio-group
+            :ethnic-friends-composition
+            {:white "Mostly white"
+             :ethnic "Mostly racial and ethnic minorities"
+             :half "About half and half"
+             :other [:span "Some other mixture. Please explain:" (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
 
 ;;;Q25.	Question:
-  (question 
-   (str "Are your friends mostly "
-        (apply str (interpose ", " (vals (political-groups subject))))
-        " or some other mixture?")
-   (bf/radio-group
-    :political-friends-composition
-    (conj
-     (vec (map (fn [[k v]] [k (str "Mostly " v)]) (political-groups subject)))
-     [ :other [:span "Some other mixture. Please explain:" (f/text-field :other-description)]]))))
+          (question 
+           (str "Are your friends mostly "
+                (apply str (interpose ", " (vals (political-groups subject))))
+                " or some other mixture?")
+           (bf/radio-group
+            :political-friends-composition
+            (conj
+             (vec (map (fn [[k v]] [k (str "Mostly " v)]) (shuffle (vec (political-groups subject)))))
+             [ :other [:span "Some other mixture. Please explain:" (f/text-field :other-description)]])))
+)))
