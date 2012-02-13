0a97e875e5f2f5d0389f1a363e7b4a66972d872a
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-02-13 14:08:20 -0600

    sending works from a gmail address

--- app/src/community_maps/mail.clj
+++ app/src/community_maps/mail.clj
@@ -6,7 +6,7 @@
   [_]
   (m/send
    (m/make-message
-    :from "jake@jakewbowers.com"
+    :from "mark.m.fredrickson@gmail.com"
     :to "mark.m.fredrickson@gmail.com"
     :subject "This is a test"
     :text-body "This is the body of the message. Yay."))
