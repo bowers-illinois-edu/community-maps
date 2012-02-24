182b5d4330c15ac5e06ecf50be5268b079434147
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-02-24 10:35:42 -0600

    adding explalin text to racial/ethnic options. closes #159

--- app/src/community_maps/screens/minorities_community.clj
+++ app/src/community_maps/screens/minorities_community.clj
@@ -18,7 +18,7 @@
            "Thinking about your local community again: is it mostly white, mostly racial and ethnic minorities, about half and half, or some other mixture of people?"
            (bf/radio-group :community-ethnic-makeup
                            {:white "Mostly white"
-                            :ethnic [:span "Mostly racial and ethnic minorities: " (f/text-field :minorities-description)]
+                            :ethnic [:span "Mostly racial and ethnic minorities. Please explain which groups: " (f/text-field :minorities-description)]
                             :half "About half and half"
                             :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
@@ -36,7 +36,7 @@
            (bf/radio-group
             :ethnic-friends-composition
             {:white "Mostly white"
-             :ethnic [:span "Mostly racial and ethnic minorities: " (f/text-field :minorities-description)]
+             :ethnic [:span "Mostly racial and ethnic minorities. Please explain which groups: " (f/text-field :minorities-description)]
              :half "About half and half"
              :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
