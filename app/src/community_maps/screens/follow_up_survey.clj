b6d746cbdf42c483b9d4d4767b6466126456e211
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-02-24 10:53:21 -0600

    replace email with checkbox, slight language change. closes #150

--- app/src/community_maps/screens/follow_up_survey.clj
+++ app/src/community_maps/screens/follow_up_survey.clj
@@ -15,8 +15,5 @@
 
      "Finally, we would like to invite you to complete a short, 5-7 minute, follow up survey in a few weeks. Everyone who completes the follow up will be entered in a drawing to win one of four iPad2 tablets."]))
   (question
-   "If you agree to be contacted again for a brief 5-7 minute survey, please enter your email in this box."
-   (f/text-field :email-address))) ; sub may
-                                        ; have supplied it earlier,
-                                        ; but rentering indicates
-                                        ; interest
+   "If you agree to be contacted again, please check the box below:"
+   (bf/labeled-checkbox :agree-to-followup "I would like to participant in another survey."))) 
