88003bacae327aee6c296da1d98907fd71c10251
Ashlea Rundlett <rundlet2@illinois.edu>
2012-05-26 15:53:20 -0500

    translation of minorities_community

--- app/src/community_maps/screens/minorities_community.clj
+++ app/src/community_maps/screens/minorities_community.clj
@@ -10,53 +10,53 @@
   [subject]
   (let [polgroups (conj (shuffle (vec (map-vals #(str % " supporters") (political-groups subject))))
                         [ :other
-                         [:span "Some other mixture. Please explain: " (f/text-field :other-description)]])]
+                         [:span "Une autre configuration. Expliquez svp:" (f/text-field :other-description)]])]
     (list
-     (directions "Now let’s look at the map of your local community again.")
+     (directions "Maintenant, jetez de nouveau un coup d’oeil à la carte de votre communauté locale.")
      (static-map-communities subject)
 
 ;;;Q22. Question:
      (question
-      "Thinking about your local community again: is it mostly white, mostly racial and ethnic minorities, about half and half, or some other mixture of people?"
+      "Quand vous avez en tête votre communauté locale, diriez-vous qu’elle est surtout composée de membres de minorités ethniques, répartis de façon égale entre les deux ou d’une autre configuration?"
       (bf/radio-group :community-ethnic-makeup
-                      {:white "Mostly white"
-                       :ethnic [:span "Mostly racial and ethnic minorities. Please explain which groups: " (f/text-field :minorities-description)]
-                       :half "About half and half"
-                       :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]}))
+                      {:white "Surtout de race blanche"
+                       :ethnic [:span "Surtout des minorités raciales et ethniques. Expliquez pour chaque groupe svp:" (f/text-field :minorities-description)]
+                       :half "Moitié-moitié"
+                       :other [:span "Une autre configuration. Expliquez svp:" (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
 ;;;
 ;;;Q23. Question:
      (question
-      "Thinking about your local community, is it mostly:"
+      "Quand vous avez en tête votre communauté locale, diriez-vous qu’elle est surtout:"
       (bf/radio-group
        :community-political-makeup
        polgroups))
 
 ;;;Q24a. Question:
      (question
-      "Now thinking more generally, are your friends mostly white, mostly racial and ethnic minorities, about half and half, or some other mixture of people?"
+      "Vos amis sont-ils surtout de race blanche, surtout membres de minorités ethniques, répartis de façon égale entre les deux ou d’une autre configuration?"
       (bf/radio-group
        :ethnic-friends-composition
-       {:white "Mostly white"
-        :ethnic [:span "Mostly racial and ethnic minorities. Please explain which groups: " (f/text-field :minorities-description)]
-        :half "About half and half"
-        :other [:span "Some other mixture. Please explain: " (f/text-field :other-description)]}))
+       {:white "Surtout de race blanche"
+        :ethnic [:span "Surtout des minorités raciales et ethniques. Expliquez pour chaque groupe svp:" (f/text-field :minorities-description)]
+        :half "Environ moitié moitié"
+        :other [:span "" (f/text-field :other-description)]}))
 ;;;What is the largest nonwhite group?
 
 ;;;Q25. Question:
      (question
-      (<< "Thinking more generally, are your friends mostly ~{(apply str (interpose \", \" (vals (political-groups subject))))} or some other mixture?")
+      (<< "Vos amis sont-ils surtout ~{(apply str (interpose \", \" (vals (political-groups subject))))} ou d’une autre configuration?")
       (bf/radio-group
        :political-friends-composition
        (conj
-        (vec (map (fn [[k v]] [k (<< "Mostly ~{v}")]) (shuffle (vec (political-groups subject)))))
-        [ :other [:span "Some other mixture. Please explain:" (f/text-field :other-description)]])))
+        (vec (map (fn [[k v]] [k (<< "Surtout ~{v}")]) (shuffle (vec (political-groups subject)))))
+        [ :other [:span "Une autre configuration. Expliquez svp:" (f/text-field :other-description)]])))
      ))
   (group-sliders
    subject
    :group-feeling-thermometers
-   (list [:p "Now we would like to get your feelings about some groups in Canadian society. For each of the following groups, we would like you to rate it with what we call a feeling thermometer."]
-         [:p "Ratings between 50 degrees and 100 degrees mean that you feel favorably and warm toward the group; ratings between 0 and 50 degrees mean that you don't feel favorably towards the group and that you don't care too much for that group. If you don't feel particularly warm or cold toward a group you would rate them at 50 degrees."])
+    (list [:p "Nous aimerions également connaître vos sentiments à l’égard de certains groupes de la société canadienne. Pour chacun des groupes suivants, nous vous demandons d'évaluer ces groupes à l’aide de ce qu’on appelle un thermomètre de sentiment."]
+         [:p "Les valeurs entre 50 et 100 degrés signifient que vous avez une opinion favorable ou positive à l’égard du groupe en question; les valeurs entre 0 et 50 degrés signifient que vous avez une opinion défavorable ou  négative envers le groupe en question; si vous n’avez pas une opinion particulièrement favorable ou défavorable envers un groupe donné, vous pouvez donner un score de 50."])
    "0" "100")
   
   no-back-button-msg)
