7f9fedb04e6464ff819ada5bf4ab3c932711ee7e
Ashlea Rundlett <rundlet2@illinois.edu>
2012-05-26 23:36:04 -0500

    translation of draw.clj

--- app/src/community_maps/screens/draw.clj
+++ app/src/community_maps/screens/draw.clj
@@ -13,18 +13,18 @@
                    #",")]
     (question
      (list
-      [:span.required "Please draw what you think of as your Local Community on the map. "]
+      [:span.required "Dessinez svp ce que vous pensez être votre communauté locale sur la carte."]
       [:ul (map #(vector :li %)
-                ["Click \"Start Drawing\" to begin drawing your community. (There is no \"right\" answer, and we would like you to draw whatever comes to mind when you think of your \"local community.\" We specifically do not provide a definition because we want to learn from you what is important and relevant.)"
-                 "Hold down the mouse button to make a line. Open shapes will be enclosed if you lift your mouse for a few 		seconds."
-                 "You may make as many shapes as you like."
-                 "Even though we have centered the map on your postal code, you should feel free to zoom in or out and to move 		the map if it does not contain the areas that you consider your local community."
-                 "If you need to zoom in or out, or move the map, after you have begun drawing, click Stop Drawing, adjust the map, and then click Start Drawing to finish drawing your community."
-                 "If you left click on a completed shape you will have the chance to delete that community."
-                 "You may Reset Map to delete all of your shapes to start over."])]
-      [:div#more-help [:a {:href "#"} "More help"]
+                ["quez \"Start Drawing\" pour commencer le dessin de votre communauté. (Il n'y a pas de \"bonne\" réponse et nous voulons que vous traciez ce qui vous vient en tête quand vous visualer votre \"local community.\" Nous ne voulons surtout pas fournir une définition car nous voulons savoir ce qui est important et pertinent pour vous.)"
+                 "Garder votre doigt sur le bouton de la souris pour tracer une ligne. Les formes ouvertes seront fermées si vous levez votre doigt pour quelques secondes."
+                 "Vous pouvez faire autant de formes que vous le voulez."
+                 "Même si nous avons centré la carte autour de votre code postal, sentez-vous libre d'utiliser le zoom ou de vous déplacer sur la carte si celle-ci ne contient pas tout le territoire que vous considérez comme partie de votre communauté locale. "
+                 "Si vous avez besoin d'utilisez le zoom ou bien de bouger la carte après avoir commencé à dessiner, cliquez sur 'Arrêter de dessiner', ajustez la carte, et ensuite cliquez sur 'Commencer à dessiner' pour terminer le dessin de votre communaute"
+                 "Si vous cliquez avec le bouton de gauche sur une zone complétée, vous aurez la chance d'effacer cette zone."
+                 " Vous pouvez réinitialiser la carte pour effacer ce que vous y avez tracé."])]
+      [:div#more-help [:a {:href "#"} "Plus d'aide"]
        [:div#more-help-hidden
         [:ul
-         [:li "Some trackpads use a single tap to indicate mouse down and then automatically keep the mouse button, as if you had your finger on the button. To release the drawing, tap the trackpad again to signal that you are done drawing."]
-         [:li "After releasing the mouse button, you have 5 seconds to start drawing again from that position. If 5 seconds elapse, or you move the mouse, the region automatically close."]]]])
+         [:li "Certains ordinateurs ont une souris intégrée qui ne nécéssite qu'un contact du doigt pour activer la souris et maintenir l'action. Pour cesser de dessiner, toucher rapidement la surface."]
+         [:li "Après avoir lâché le bouton de la souris, vous avez 5 secondes pour dessiner de nouveau de la position où vous êtes. Au bout de ces 5 secondes, ou si vous bougez la souris, la région se referme automatiquement. "]]]])
      (scribble-map :community lat lng (:drawing-zoom subject)))))
