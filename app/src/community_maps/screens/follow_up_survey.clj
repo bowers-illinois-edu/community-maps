630640d7fc28acf65546071b5e1f5d0572a004a4
Ashlea Rundlett <rundlet2@illinois.edu>
2012-05-26 23:39:01 -0500

    translation of follow_up_survey.clj

--- app/src/community_maps/screens/follow_up_survey.clj
+++ app/src/community_maps/screens/follow_up_survey.clj
@@ -13,7 +13,7 @@
 
      "Nous désirons vous rappeler encore une fois que vos réponses demeureront confidentielles et seront uniquement utilisées à des fins de recherche académique."
 
-     "En conclusion, nous aimerions vous inviter à compléter un court questionnaire d'une durée de 5 à 7 minutes dans quelques semaines. Tous ceux qui accepteront de remplir ce court questionnaire additionel courent la chance de gagner un de quatre iPad 3 tirés au hasard."]))
+     "En conclusion, nous aimerions vous inviter à compléter un court questionnaire d'une durée de 5 à 7 minutes dans quelques semaines. Tous ceux qui accepteront de remplir ce court questionnaire additionel courent la chance de gagner un de quatre iPad 3 tirés au hasard. "]))
   (question
    "Si vous acceptez d'être contacté de nouveau, svp cochez la case ci-dessous:"
    (bf/labeled-checkbox :agree-to-followup "J'aimerais participer à une autre enquête."))) 
