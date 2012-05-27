9ddd35e3970e5ad27e51b73f9868dbda1cf2f5d7
Ashlea Rundlett <rundlet2@illinois.edu>
2012-05-26 23:55:56 -0500

    translation for address.clj

--- app/src/community_maps/screens/address.clj
+++ app/src/community_maps/screens/address.clj
@@ -11,67 +11,64 @@
   
 ;;;Q1.
   (question
-   "In a moment, we will ask you to look at where you live on a map.
-Please enter your postal code in the box and then click the \"Find On Map\" button. 
-
-If the map does not look right, please try entering the postal code (or an intersection or address) again. (Reminder: These surveys are confidential, and your postal code will not be used for any purpose aside from this survey.)"
+   "Dans un instant, nous vous demanderons de regarder sur la carte et de nous dire où vous habitez. Insérez svp votre code postal dans la boîte puis cliquez sur le bouton \"Trouver sur la carte\". Si la carte vous semble incorrect, essayez de réinsérer votre code postal (ou bien une intersection ou une adresse).  (Note: Ce genre d’enquête est anonyme. Votre adresse ne sera donc jamais utilisée pour d'autres fins que cette enquête.) "
             (f/with-group :address-finder
               [:div.map-find-address
                (f/hidden-field {:class "latlng"} :latlng)
-               (f/text-field {:class "address"} :address (:address subject))
-               (add-class (button "Find on map") "update")
+               (f/text-field {:class "adresse"} :address (:address subject))
+               (add-class (button "Trouver sur la carte") "update")
                [:div.map-canvas {:style "height: 400px; width: 100%;"}]
                  ])) ; if preloaded 
 
 
 ;;;Q2.	Question:
   [:div#how-long-lived
-   (question "How long have you lived in your current home?"
+   (question "Depuis combien de temps demeurez-vous à votre domicile actuel?"
              (bf/radio-group :length-of-residence
-                             {:less-than-one "Less than one year"
-                              :x-years [:span (f/text-field :x-years) " years (please fill in the correct number)"]
-                              :all-my-life "All my life"}))]
+                             {:less-than-one "Moins d'un an"
+                              :x-years [:span (f/text-field :x-years) "années (insérez le nombre d'années svp)"]
+                              :all-my-life "Toute ma vie"}))]
 ;;;Q3.	Question: 
   [:div#live-followup
    (question
-    "What city and province did you live in before moving to this home? Please select the best description and fill in the name of the location below."
+    "Dans quelle municipalité et quelle province viviez-vous avant d’aménager dans votre domicile actuel? Sélectionnez la meilleure description possible et compléter la localisation ci-dessous."
     (bf/radio-group :other-residence
                     (map (fn [[k [v1 v2]]] [k [:span v1 " " [:span.followup v2 " " (f/with-group k (f/text-field :details))]]])
-                         {:city ["Elsewhere in the same city" " -- Please tell us the postal code if you recall it."]
-                          :province ["Elsewhere in the same province" " -- Please tell us the name of the city or the postal code."]
-                          :other ["In another province" " -- Please tell us the name of the city or the postal code."]
-                          :ex-canada ["Outside Canada" " -- Please tell us the name of the country."]})))]
+                         {:city ["Ailleurs dans la ville" "--S'il vous plaît dites-nous le code postal si vous vous en souvenez."]
+                          :province ["Ailleurs dans la même province" " -- S'il vous plaît dites-nous le nom de la ville ou le code postal."]
+                          :other ["Dans une autre province" " -- S'il vous plaît dites-nous le nom de la ville ou le code postal."]
+                          :ex-canada ["À l'extérieur du Canada" " -- S'il vous plaît dites moi le nom du pays"]})))]
 
 ;;;Q4.	Question:
   (question 
-   "Do you or your family own your own home/apartment, pay rent or what?"
+   "Est-ce que vous ou votre famille êtes propriétaires, locataires ou autres?"
    (bf/radio-group
     :rent-own
-    {:yes "We own"
-     :rent "We rent"
-     :other "We have another arrangement"}))
+    {:yes "Nous sommes propriétaires"
+     :rent "Nous sommes locataires"
+     :other "Nous avons un autre arrangement"}))
 
   (when (not (= "control" (:feel-close-to subject)))
     (list
      (question
       (<<
-       "Thinking about where you live, how close do you feel to ~{(when (not (= \"Canada\" (:feel-close-to subject))) \"your \")} ~{(:feel-close-to subject)}?")
+       "En ayant en tête où vous vivez, à quel point vous sentez-vous proche de ~{(when (not (= \"Canada\" (:feel-close-to subject))) \"your \")} ~{(:feel-close-to subject)}?")
       (bf/radio-group
        :feel-close-to-district
-       {:very-close "Very close"
-        :close "Close"
-        :not-close "Not close"
-        :not-close-at-all "Not close at all"}))
+       {:very-close "Très proche"
+        :close "Proche"
+        :not-close "Pas proche"
+        :not-close-at-all "Pas proche du tout"}))
      (question
       (<<
-       "If you could improve your work or living conditions, how willing or unwilling would you be to move to another 
-       ~{(if (= \"Canada\" (:feel-close-to subject)) \"country\"  (:feel-close-to subject))}?")
+       "Si vous pouviez améliorer votre travail ou vos conditions de vie, à quel point seriez-vous prêt à déménager dans un/e autre 
+              ~{(if (= \"Canada\" (:feel-close-to subject)) \"country\"  (:feel-close-to subject))}?")
       (bf/radio-group
        :willing-to-move
-       {:very-willing "Very willing"
-        :fairly-willing "Fairly willing"
-        :neutral "Neither willing nor unwilling"
-        :fairly-unwilling "Fairly unwilling"
-        :very-unwilling "Very unwilling"}))))
+       {:very-willing "Très désireux"
+        :fairly-willing "Assez désireux"
+        :neutral "Ni l'un ni l'autre"
+        :fairly-unwilling "Assez peu désireux"
+        :very-unwilling "Pas désireux du tout"}))))
 
   no-back-button-msg)
