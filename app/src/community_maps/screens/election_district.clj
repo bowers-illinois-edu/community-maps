d348f2e0ac23a787fabb538fea1d89f76e56a3bb
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-06-08 16:44:42 -0500

    adding some meat to the election drawing condition

--- app/src/community_maps/screens/election_district.clj
+++ app/src/community_maps/screens/election_district.clj
@@ -10,8 +10,8 @@
      (directions
       "Imagine you are in charge of drawing the district boundaries for your riding. This district will define who is eligible to represent your community in the House of Commons."
       (if (= "sortition" (:election-district-type subject))
-        "However, imagine that instead of electing a candidate, any citizen 18 years or older in your riding can put his or her name up for consideration, and one person will be <strong>randomly selected</strong> to serve."
-        "This district also defines who can vote for candidates for office.")
+        "However, imagine that instead of electing a candidate, any citizen 18 years or older in your riding can put his or her name up for consideration, and one person will be <strong>randomly selected</strong> to represent this district."
+        "Any citizen 18 years or older can stand for election, and the candidate with the largest share of the vote will be elected to represent this district. This district also defines who can vote for candidates for office.")
       "On the map below, please draw a district that you think would lead to the best possible person being sent to Ottawa to represent your community in the House of Commons.")
      (scribble-map :outcome lat lng 12))))
 
