47243265cf589d834d27b6afdfb3b9b23e0f8455
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-07-02 11:20:40 -0500

    added race q to demographics page, closes #222

--- app/src/community_maps/screens/demographics.clj
+++ app/src/community_maps/screens/demographics.clj
@@ -3,7 +3,8 @@
         [community-maps tags util]
         clojure.contrib.strint)
   (:require
-    [burp.forms :as bf]))
+   [burp.forms :as bf]
+   [hiccup.form-helpers :as f]))
 
 (defscreen demographics
   [subject]
@@ -25,4 +26,23 @@
      :40-60 "$40,000 to $59,999"
      :60-100 "$60,000 to $99,999"
      :100-200 "$100,000 to $199,999"
-     :more200 "More than $200,000"})))
+     :more200 "More than $200,000"}))
+
+  (question
+   "The 2011 Canadian Census asks the following question in order to collect information in accordance with the Employment Equity Act and its Regulations and Guidelines to support programs that promote equal opportunity for everyone to share in the social, cultural, and economic life of Canada.<br/><br/>
+You can mark more than one or specify, if applicable. Are you:"
+
+   (bf/radio-group
+    :race
+    [[:white "White"]
+     [:southasian "South Asian (e.g., East Indian, Pakistani, Sri Lankan, etc.)"]
+     [:chinese "Chinese"]
+     [:black "Black"]
+     [:filipino "Filipino"]
+     [:latinam "Latin American"]
+     [:arab "Arab"]
+     [:southeastasian "Southeast Asian (e.g., Vietnamese, Cambodian, Malaysian, Laotian, etc.)"]
+     [:westasian "West Asian (e.g., Iranian, Afghan, etc.)"]
+     [:korean "Korean"]
+     [:japanese "Japanese"]
+     [:other [:span  "Other. Please specify: " (f/text-field :other-description)]]])))
