f0c17576a201b20b5fbbfd32b3a0c730b00d059c
jwbowers <jake@jakebowers.org>
2012-08-13 15:45:25 -0400

    FIxed #234

--- app/src/community_maps/screens/vmexperiment.clj
+++ app/src/community_maps/screens/vmexperiment.clj
@@ -8,15 +8,24 @@
 
 (defscreen vmexperiment
   [subject]
-  (question
+
+           (ordered-choice
+             :increase-immigration
+  ;;(question
    (str "According to projections by the census, and largely as a result of immigration, the number of visible minorities is going to reach 30% of the Canadian population in the next 20 years. This is about double the current size. "
     (when (= "threat" (:vismin-experiment subject))
       "Many political leaders worry that this rapid change threatens Canadian identity. ")
-    "<br/><br/>Do you think the number of immigrants from foreign countries who are permitted to come to Canada to live should:")
-   (bf/radio-group
-    :increase-immigration
-    { :increase-little "Increase a little"
-     :increase-lot "Increase a lot"
-     :decrease-little "Decrease a little"
-     :decrease-lot "Decrease a lot"
-     :same "Stay about the same"})))
+        "<br> <br> Do you think the number of immigrants from foreign countries who are permitted to come to Canada to live should be increased a little, increased a lot, decreased a little, decreased a lot, or left the same as it is now?")
+   ;; (bf/radio-group
+   ;;   :increase-immigration
+    ;;  { 
+      (keypairs 
+       :increase-lot "Increased a lot"
+       :increase-little "Increased a little"
+       :same "Left the same as it is now"
+       :decrease-little "Decreased a little"
+       :decrease-lot "Decreased a lot"
+  ;;     }
+      )
+    )
+           )
